dbd233a0173aae28f355a9f61141f96e523e2587
Yerzhan <yerzhan>
2013-12-19 10:04:15 +0600

    .

--- release/make.sh
+++ release/make.sh
@@ -2,6 +2,7 @@
 rm ArabicDictionary.zip
 cp ../dist/ArabicDictionary.jar .
 cp ../misc/'Грамматика. Справочник. Часть 1.pdf' 'Grammar. Reference Guide. Part 1.pdf'
+cp ../misc/'Грамматика. Справочник. Часть 2.pdf' 'Grammar. Reference Guide. Part 2.pdf'
 cp ../misc/Таджуиид.pdf Tajweed.pdf
 zip ArabicDictionary * -x make.sh
 rm *.jar *.pdf
